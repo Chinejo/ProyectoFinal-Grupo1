0f4cd9f4b4865f8380ed9d78b41a36e6725eca42
Brisa <229006269+brisadaianajbj-dev@users.noreply.github.com>
2025-09-27 18:32:30 -0300

    se puso cada imagen dentro de un bloque contenedor

--- Landing-Page/src/componentes/Galeria.jsx
+++ Landing-Page/src/componentes/Galeria.jsx
@@ -9,19 +9,31 @@ const Galeria = () => {
   return (
     <section>
       <h2>Galería</h2>
-      <p>Algunos sectores de nuestra institución</p>
-      <img src={bibliotecaImg} alt="Biblioteca" />
-      <p>Nuestra biblioteca</p>
-      <img src={actividadesImg} alt="Alumnos realizando teatro" />
-      <p>Teatro realizado por alumnos y profesores</p>
-      <img src={escenarioImg} alt="Escenario" />
-      <p>Escenario para eventos</p>
-      <img src={patioImg} alt="Patio" />
-      <p>Patio central</p>
-      <img src={mastilImg} alt="Mástil" />
-      <p>Mástil en el patio exterior</p>
-      <img src={canchaImg} alt="Canchas" />
-      <p>Canchas de fútbol, voley y básquet</p>
+      <p>Estos son algunos sectores de nuestra institución</p>
+        <div>
+            <img src={bibliotecaImg} alt="Biblioteca" />
+            <p>Nuestra biblioteca</p>
+        </div>
+        <div>
+            <img src={actividadesImg} alt="Alumnos realizando teatro" />
+            <p>Teatro realizado por alumnos y profesores</p>
+        </div>
+        <div>
+            <img src={escenarioImg} alt="Escenario" />
+            <p>Escenario para eventos</p>
+        </div>
+        <div>
+            <img src={patioImg} alt="Patio" />
+            <p>Patio central</p>   
+        </div>
+        <div>
+            <img src={mastilImg} alt="Mástil" />
+            <p>Mástil en el patio exterior</p>
+        </div>
+        <div>
+            <img src={canchaImg} alt="Canchas" />
+            <p>Canchas de fútbol, voley y básquet</p>
+        </div>
     </section>
   )
 }
