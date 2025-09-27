74c6f27581486171a2b71936d63edf4187e3a1a5
Emilse Ibarra <218144513+emilseibarra@users.noreply.github.com>
2025-09-27 12:46:17 -0300

    Se añade el contenido de la Oferta académica

--- Landing-Page/src/componentes/OfertaAcademica.jsx
+++ Landing-Page/src/componentes/OfertaAcademica.jsx
@@ -2,9 +2,94 @@ import React from 'react'
 
 function OfertaAcademica() {
   return (
-    <div>
-      <h2>hola</h2>
-    </div>
+    <section>
+      <h2>OFERTA ACADÉMICA</h2>
+      <section>
+        <header>
+          <h3>Turno mañana y tarde</h3>
+        </header>
+
+        <ul>
+          <li>
+            <section>
+              <h4>Ciencias Sociales</h4>
+              <p>Esta modalidad fomenta el pensamiento crítico, la comprensión histórica y el análisis de fenómenos sociales. Ideal para estudiantes interesados en la ciudadanía, la comunicación y el trabajo comunitario.</p> 
+              <br/>
+              <dl>
+                <dt>Duración:</dt><dd>6 años</dd>
+                <dt>Horarios:</dt><dd>14:00 a 18:00</dd> 
+                <dt>Edad:</dt><dd>De 12 años a 17 años</dd> 
+                <dt>Alcance:</dt> <dd>...</dd>
+              </dl>
+            </section>
+          </li>
+
+          <li>
+            <section>
+              <h4>Ciencias Naturales</h4>
+              <p>Esta modalidad promueve el desarrollo del pensamiento lógico y científico a través del estudio de la biología, química, física y matemática. Está orientada a estudiantes con interés en la investigación, el medio ambiente y las ciencias aplicadas.</p> 
+              <br/>
+              <dl>
+                <dt>Duración:</dt><dd>6 años</dd>
+                <dt>Horarios:</dt><dd>14:00 a 18:00</dd> 
+                <dt>Edad:</dt><dd>De 12 años a 17 años</dd> 
+                <dt>Alcance:</dt> <dd>...</dd>
+              </dl>
+            </section>
+          </li>
+
+          <li>
+            <section>
+              <h4>Economía</h4>
+              <p>Esta orientación introduce a los estudiantes en el mundo de la administración, la contabilidad y las finanzas. Se enfoca en el desarrollo de habilidades organizativas, análisis económico y gestión de recursos.</p> 
+              <br/>
+              <dl>
+                <dt>Duración:</dt><dd>6 años</dd>
+                <dt>Horarios:</dt><dd>14:00 a 18:00</dd> 
+                <dt>Edad:</dt><dd>De 12 años a 17 años</dd> 
+                <dt>Alcance:</dt> <dd>...</dd>
+              </dl>
+            </section>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <header>
+          <h3>Turno noche</h3>
+        </header>
+
+        <ul>
+          <li>
+            <section>
+              <h4>Economía (Integrado)</h4>
+              <p>Modalidad pensada para jóvenes menores de 17 años cumplidos al 30 de junio. Ofrece formación en ciencias económicas y administración, con un plan de estudios adaptado a sus necesidades.</p>
+              <br/>
+              <dt>
+                <dt>Duración:</dt><dd>4 años</dd>
+                <dt>Horarios:</dt><dd>20:00 a 20:00</dd> 
+                <dt>Edad:</dt><dd>Menores a 17 años</dd> 
+                <dt>Alcance:</dt> <dd>...</dd>
+              </dt>
+            </section>
+          </li>
+
+          <li>
+            <section>
+              <h4>Economía (Integrado +17 años)</h4>
+              <p>Diseñada para personas mayores de 17 años cumplidos al 30 de junio, esta modalidad brinda una formación sólida en economía y gestión, con un enfoque práctico y flexible.</p>
+              <br/>
+              <dt>
+                <dt>Duración:</dt><dd>4 años</dd>
+                <dt>Horarios:</dt><dd>20:00 a 20:00</dd> 
+                <dt>Edad:</dt><dd>Mayores a 17 años</dd> 
+                <dt>Alcance:</dt> <dd>...</dd>
+              </dt>
+            </section>
+          </li>
+        </ul>
+      </section>
+    </section>
   )
 }
 
