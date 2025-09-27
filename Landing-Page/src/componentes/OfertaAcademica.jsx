620b67a5914cbf70833fdce019adbcc9836f66ff
Emilse Ibarra <218144513+emilseibarra@users.noreply.github.com>
2025-09-27 15:31:52 -0300

    Se agregan datos faltantes

--- Landing-Page/src/componentes/OfertaAcademica.jsx
+++ Landing-Page/src/componentes/OfertaAcademica.jsx
@@ -11,45 +11,42 @@ function OfertaAcademica() {
 
         <ul>
           <li>
-            <section>
+            <article>
               <h4>Ciencias Sociales</h4>
               <p>Esta modalidad fomenta el pensamiento crítico, la comprensión histórica y el análisis de fenómenos sociales. Ideal para estudiantes interesados en la ciudadanía, la comunicación y el trabajo comunitario.</p> 
-              <br/>
               <dl>
                 <dt>Duración:</dt><dd>6 años</dd>
                 <dt>Horarios:</dt><dd>14:00 a 18:00</dd> 
                 <dt>Edad:</dt><dd>De 12 años a 17 años</dd> 
-                <dt>Alcance:</dt> <dd>...</dd>
+                <dt>Alcance:</dt> <dd>Nacional y Mercosur</dd>
               </dl>
-            </section>
+            </article>
           </li>
 
           <li>
-            <section>
+            <article>
               <h4>Ciencias Naturales</h4>
               <p>Esta modalidad promueve el desarrollo del pensamiento lógico y científico a través del estudio de la biología, química, física y matemática. Está orientada a estudiantes con interés en la investigación, el medio ambiente y las ciencias aplicadas.</p> 
-              <br/>
               <dl>
                 <dt>Duración:</dt><dd>6 años</dd>
                 <dt>Horarios:</dt><dd>14:00 a 18:00</dd> 
                 <dt>Edad:</dt><dd>De 12 años a 17 años</dd> 
-                <dt>Alcance:</dt> <dd>...</dd>
+                <dt>Alcance:</dt> <dd>Nacional y Mercosur</dd>
               </dl>
-            </section>
+            </article>
           </li>
 
           <li>
-            <section>
+            <article>
               <h4>Economía</h4>
               <p>Esta orientación introduce a los estudiantes en el mundo de la administración, la contabilidad y las finanzas. Se enfoca en el desarrollo de habilidades organizativas, análisis económico y gestión de recursos.</p> 
-              <br/>
               <dl>
                 <dt>Duración:</dt><dd>6 años</dd>
                 <dt>Horarios:</dt><dd>14:00 a 18:00</dd> 
                 <dt>Edad:</dt><dd>De 12 años a 17 años</dd> 
-                <dt>Alcance:</dt> <dd>...</dd>
+                <dt>Alcance:</dt> <dd>Nacional y Mercosur</dd>
               </dl>
-            </section>
+            </article>
           </li>
         </ul>
       </section>
@@ -61,31 +58,29 @@ function OfertaAcademica() {
 
         <ul>
           <li>
-            <section>
+            <article>
               <h4>Economía (Integrado)</h4>
               <p>Modalidad pensada para jóvenes menores de 17 años cumplidos al 30 de junio. Ofrece formación en ciencias económicas y administración, con un plan de estudios adaptado a sus necesidades.</p>
-              <br/>
-              <dt>
+              <dl>
                 <dt>Duración:</dt><dd>4 años</dd>
-                <dt>Horarios:</dt><dd>20:00 a 20:00</dd> 
+                <dt>Horarios:</dt><dd>19:45 a 00:00</dd> 
                 <dt>Edad:</dt><dd>Menores a 17 años</dd> 
-                <dt>Alcance:</dt> <dd>...</dd>
-              </dt>
-            </section>
+                <dt>Alcance:</dt> <dd>Nacional y Mercosur</dd>
+              </dl>
+            </article>
           </li>
 
           <li>
-            <section>
+            <article>
               <h4>Economía (Integrado +17 años)</h4>
               <p>Diseñada para personas mayores de 17 años cumplidos al 30 de junio, esta modalidad brinda una formación sólida en economía y gestión, con un enfoque práctico y flexible.</p>
-              <br/>
-              <dt>
+              <dl>
                 <dt>Duración:</dt><dd>4 años</dd>
-                <dt>Horarios:</dt><dd>20:00 a 20:00</dd> 
+                <dt>Horarios:</dt><dd>19:45 a 00:00</dd> 
                 <dt>Edad:</dt><dd>Mayores a 17 años</dd> 
-                <dt>Alcance:</dt> <dd>...</dd>
-              </dt>
-            </section>
+                <dt>Alcance:</dt> <dd>Nacional y Mercosur</dd>
+              </dl>
+            </article>
           </li>
         </ul>
       </section>
