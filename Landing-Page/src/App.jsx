8562c8190c16818bddb6d3f117df2a3bd2cfa49f
Ale <197334889+Chinejo@users.noreply.github.com>
2025-09-28 23:22:58 -0300

    fixear merge de footer en dev

--- Landing-Page/src/App.jsx
+++ Landing-Page/src/App.jsx
@@ -1,20 +1,16 @@
 import OfertaAcademica from './componentes/OfertaAcademica'
-import Galeria from './componentes/Galeria';
+import Galeria from './componentes/Galeria'
 import BotonInicio from './components/Boton-inicio' 
-import BotonContacto from './components/Boton-contacto';
+import BotonContacto from './components/Boton-contacto'
+import Footer from "./components/Footer"
 
-const App = () => {
-import Footer from "./components/Footer";
 function App() {
   return (
-    <div>
+    <>
       <OfertaAcademica />
       <Galeria />
       <BotonInicio />
       <BotonContacto />
-    </div>
-  )
-    <>
       <Footer />
     </>
   );
