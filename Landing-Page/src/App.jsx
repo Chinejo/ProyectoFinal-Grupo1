179bd36db86dd361421190490e751340c6da1fb6
Ale <197334889+Chinejo@users.noreply.github.com>
2025-09-28 23:19:54 -0300

    fixear el merge de oferta academica a dev

--- Landing-Page/src/App.jsx
+++ Landing-Page/src/App.jsx
@@ -1,24 +1,12 @@
-import React from 'react'
 import OfertaAcademica from './componentes/OfertaAcademica'
-
-function App() {
-  return (
-    <div>
-        <OfertaAcademica />
-    </div>
-  )
-}
-
-export default App
-import React from 'react';
 import Galeria from './componentes/Galeria';
-import React from 'react'
 import BotonInicio from './components/Boton-inicio' 
 import BotonContacto from './components/Boton-contacto';
 
 const App = () => {
   return (
     <div>
+      <OfertaAcademica />
       <Galeria />
       <BotonInicio />
       <BotonContacto />
