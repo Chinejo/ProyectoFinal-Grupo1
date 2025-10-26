b97588e6292e47b3cdb9e047b4f6710924e598ba
Ale <197334889+Chinejo@users.noreply.github.com>
2025-10-26 17:39:42 -0300

    Añadido botón de menú hamburguesa para pantallas chicas, falta pulirlo

--- Landing-Page/src/components/NavBar.jsx
+++ Landing-Page/src/components/NavBar.jsx
@@ -8,58 +8,67 @@ import '../styles/NavBar.css';
 
 function NavBar() {
   return (
-    <Navbar className="navbar-main py-1" sticky="top">
+    <Navbar expand="lg" className="navbar-main py-1" sticky="top">
       <Row className="w-100">
         <Container fluid>
-          <Navbar.Brand href="#inicio" className="d-flex align-items-center px-3">
-            <img
-              src={Logo}
-              alt="Logo de la Escuela de Comercio República de Panamá"
-              height="50"
-              className="m-3"
-            />
-            <span className="navbar-title fw-bold d-inline mb-0">
-              Escuela de Comercio<br className="d-block d-md-none" /> República de Panamá
-            </span>
-          </Navbar.Brand>
-        </Container>
-
-        <Container fluid>
-          <Row>
-            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
-              <Nav.Link 
-                href="#sobre-nosotros" 
-                className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
-              >
-                Sobre Nosotros
-              </Nav.Link>
+          <Row className="align-items-center">
+            <Col xs={10} lg={12}>
+              <Navbar.Brand href="#inicio" className="d-flex align-items-center px-3">
+                <img
+                  src={Logo}
+                  alt="Logo de la Escuela de Comercio República de Panamá"
+                  height="50"
+                  className="m-2"
+                />
+                <span className="navbar-title fw-bold d-inline mb-0">
+                  Escuela de Comercio<br className="d-block d-md-none" /> República de Panamá
+                </span>
+              </Navbar.Brand>
             </Col>
-            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
-              <Nav.Link 
-                href="#info-planes" 
-                className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
-              >
-                Información de Planes
-              </Nav.Link>
-            </Col>
-            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
-              <Nav.Link 
-                href="#galeria" 
-                className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
-              >
-                Galería
-              </Nav.Link>
-            </Col>
-            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
-              <Nav.Link 
-                href="#mapa-ubicacion" 
-                className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
-              >
-                Ubicación
-              </Nav.Link>
+            <Col xs={2} className="d-lg-none d-flex justify-content-end pe-3">
+              <Navbar.Toggle aria-controls="navbar-nav" className="btn-sm bg-transparent border-0 text-white" />
             </Col>
           </Row>
         </Container>
+
+        <Navbar.Collapse id="navbar-nav">
+          <Container fluid>
+            <Row>
+              <Col xxl={3} xl={3} lg={3} xs={12} className="d-flex justify-content-center py-1">
+                <Nav.Link 
+                  href="#sobre-nosotros" 
+                  className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
+                >
+                  Sobre Nosotros
+                </Nav.Link>
+              </Col>
+              <Col xxl={3} xl={3} lg={3} xs={12} className="d-flex justify-content-center py-1">
+                <Nav.Link 
+                  href="#info-planes" 
+                  className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
+                >
+                  Información de Planes
+                </Nav.Link>
+              </Col>
+              <Col xxl={3} xl={3} lg={3} xs={12} className="d-flex justify-content-center py-1">
+                <Nav.Link 
+                  href="#galeria" 
+                  className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
+                >
+                  Galería
+                </Nav.Link>
+              </Col>
+              <Col xxl={3} xl={3} lg={3} xs={12} className="d-flex justify-content-center py-1">
+                <Nav.Link 
+                  href="#mapa-ubicacion" 
+                  className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
+                >
+                  Ubicación
+                </Nav.Link>
+              </Col>
+            </Row>
+          </Container>
+        </Navbar.Collapse>
       </Row>
     </Navbar>
   );
