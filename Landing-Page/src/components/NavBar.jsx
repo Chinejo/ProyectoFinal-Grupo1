ffbddd4cedd891ada9d0558c45247be16cb19554
Ale <197334889+Chinejo@users.noreply.github.com>
2025-10-26 04:29:12 -0300

    Corregido jsx y css para hacer uso de col, row y container en lugar de media queries

--- Landing-Page/src/components/NavBar.jsx
+++ Landing-Page/src/components/NavBar.jsx
@@ -1,4 +1,6 @@
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/LogoEscolar.jpg';
@@ -6,33 +8,40 @@ import '../styles/NavBar.css';
 
 function NavBar() {
   return (
-    <div className="navbar-wrapper">
-      <Navbar expand="lg" className="navbar-main bg-body-tertiary">
-        <Container className="navbar-top-container">
-          <Navbar.Brand href="#inicio" className="navbar-brand-section">
+    <Navbar className="navbar-main" sticky="top">
+      <Row>
+        {/* Primer container: Logo y título hacia el lado izquierdo */}
+        <Container fluid>
+          <Navbar.Brand href="#inicio" className="navbar-brand-section d-flex align-items-center">
             <img
               src={Logo}
               alt="Logo de la Escuela de Comercio República de Panamá"
               height="50"
-              className="navbar-logo"
+              className="navbar-logo m-3"
             />
-            <span className="navbar-title">Escuela de Comercio República de Panamá</span>
+            <span className="navbar-title d-inline mb-0">Escuela de Comercio<br className="d-block d-md-none" /> República de Panamá</span>
           </Navbar.Brand>
-
-          <Navbar.Toggle aria-controls="basic-navbar-nav" className="navbar-toggle-mobile" />
         </Container>
 
-        <Navbar.Collapse id="basic-navbar-nav" className="navbar-menu-section">
-          <Container>
-            <Nav className="mx-auto navbar-nav-custom">
+        {/* Segundo container: Elementos del nav distribuidos uniformemente */}
+        <Container fluid>
+          <Row>
+            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
               <Nav.Link href="#sobre-nosotros" className="nav-button">Sobre Nosotros</Nav.Link>
+            </Col>
+            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
               <Nav.Link href="#info-planes" className="nav-button">Información de Planes</Nav.Link>
+            </Col>
+            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
               <Nav.Link href="#galeria" className="nav-button">Galería</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+            </Col>
+            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
+              <Nav.Link href="#mapa-ubicacion" className="nav-button">Ubicación</Nav.Link>
+            </Col>
+          </Row>
+        </Container>
+      </Row>
+    </Navbar>
   );
 }
 
