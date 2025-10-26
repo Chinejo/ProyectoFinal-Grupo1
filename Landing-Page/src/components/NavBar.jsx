9af9aca26ebc17e6b504529195aa79fe717e9eb3
Ale <197334889+Chinejo@users.noreply.github.com>
2025-10-26 16:21:29 -0300

    Ajustado el alto del nav bar para ser más estrecho

--- Landing-Page/src/components/NavBar.jsx
+++ Landing-Page/src/components/NavBar.jsx
@@ -8,14 +8,14 @@ import '../styles/NavBar.css';
 
 function NavBar() {
   return (
-    <Navbar className="navbar-main py-3" sticky="top">
+    <Navbar className="navbar-main py-1" sticky="top">
       <Row className="w-100">
-        <Container fluid className='pb-3'>
+        <Container fluid>
           <Navbar.Brand href="#inicio" className="d-flex align-items-center px-3">
             <img
               src={Logo}
               alt="Logo de la Escuela de Comercio República de Panamá"
-              height="65"
+              height="50"
               className="m-3"
             />
             <span className="navbar-title fw-bold d-inline mb-0">
@@ -26,34 +26,34 @@ function NavBar() {
 
         <Container fluid>
           <Row>
-            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center pb-2">
+            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
               <Nav.Link 
                 href="#sobre-nosotros" 
-                className="nav-button fw-bold px-4 py-2 rounded-pill border border-2 border-transparent text-center text-nowrap"
+                className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
               >
                 Sobre Nosotros
               </Nav.Link>
             </Col>
-            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center pb-2">
+            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
               <Nav.Link 
                 href="#info-planes" 
-                className="nav-button fw-bold px-4 py-2 rounded-pill border border-2 border-transparent text-center text-nowrap"
+                className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
               >
                 Información de Planes
               </Nav.Link>
             </Col>
-            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center pb-2">
+            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
               <Nav.Link 
                 href="#galeria" 
-                className="nav-button fw-bold px-4 py-2 rounded-pill border border-2 border-transparent text-center text-nowrap"
+                className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
               >
                 Galería
               </Nav.Link>
             </Col>
-            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center pb-2">
+            <Col xxl={3} xl={3} lg={3} md={6} sm={6} className="d-flex justify-content-center">
               <Nav.Link 
                 href="#mapa-ubicacion" 
-                className="nav-button fw-bold px-4 py-2 rounded-pill border border-2 border-transparent text-center text-nowrap"
+                className="nav-button fw-bold px-4 py-1 rounded-pill text-center text-nowrap"
               >
                 Ubicación
               </Nav.Link>
