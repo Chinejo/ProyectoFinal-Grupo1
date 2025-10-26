2b664c1d8dc544c320584926606751d364b3e498
Emilse Ibarra <218144513+emilseibarra@users.noreply.github.com>
2025-10-26 19:20:40 -0300

    se coloca la estructura del carrusel

--- Landing-Page/src/components/Carrusel.jsx
+++ Landing-Page/src/components/Carrusel.jsx
@@ -18,7 +18,7 @@ function ControlledCarousel() {
       <Carousel.Item>
         <ExampleCarouselImage text="First slide" />
         <Carousel.Caption>
-          <h3>First slide label</h3>
+          <h3>First slide label</h3> 
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
