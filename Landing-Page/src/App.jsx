f4a82b6b85cb810b5b2a660552d03fe438336d4d
Pablo Soria <227870924+pablogabrielsoria94-sys@users.noreply.github.com>
2025-11-17 01:21:11 -0300

    mejoro estilo boton-contacto

--- Landing-Page/src/App.jsx
+++ Landing-Page/src/App.jsx
@@ -4,7 +4,7 @@ import HeroSection from './components/HeroSection.jsx';
 import SobreNosotros from './components/SobreNosotros.jsx'
 import OfertaAcademica from './components/OfertaAcademica.jsx'
 import Galeria from './components/Galeria.jsx'
-import Botoninscripcion from './components/Boton-inscripcion.jsx'
+import BotonInscripcion from './components/Boton-inscripcion.jsx'
 import BotonContacto from './components/Boton-contacto.jsx'
 import Footer from "./components/Footer.jsx"
 
